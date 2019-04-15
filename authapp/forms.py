ec29f4b3450472d4292eb5b1eece66a9167e5c36
kosticmostic <kosticmostic@yandex.ru>
2019-04-15 14:41:15 +0300

    less4full

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from .models import ShopUser
+from django import forms
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -11,3 +12,38 @@ class ShopUserLoginForm(AuthenticationForm):
         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'age', 'password1', 'password2', 'email', 'first_name')
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class UpdateForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'age', 'password', 'email', 'first_name')
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from .forms import ShopUserLoginForm
+from .forms import ShopUserLoginForm, RegisterForm, UpdateForm
 from django.contrib import auth
 from django.urls import reverse
 from django.http import HttpResponse, HttpRequest
@@ -25,4 +25,45 @@ def login(request: HttpRequest):
 
 def logout(request: HttpRequest):
     auth.logout(request)
-    return HttpResponseRedirect(reverse('main'))
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request: HttpRequest):
+    tittle = 'регистрация'
+
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+
+    else:
+        register_form = RegisterForm()
+
+    content = {
+        'tittle': tittle,
+        'reg_form': register_form
+    }
+    return render(request, 'authapp/register.html', content)
+
+
+def edit(request: HttpResponse):
+    tittle = 'профиль'
+
+    if request.method == 'POST':
+        update_form = UpdateForm(request.POST, instance=request.user)
+
+        if update_form.is_valid():
+            update_form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        update_form = UpdateForm(instance=request.user)
+
+    content = {
+        'tittle': tittle,
+        'update_form': update_form
+    }
+
+    return render(request, 'authapp/edit.html', content)
+
