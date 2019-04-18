caf1ee59f0308b1d0aeb010cc5423c293d390d51
kosticmostic <kosticmostic@yandex.ru>
2019-04-18 18:13:42 +0300

    less5part1

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,6 @@ import mainapp.views as controller
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', controller.products, name='index'),
+    path('', controller.products, name='index_category'),
     path('<int:pk>/', controller.products, name='category'),
 ]
